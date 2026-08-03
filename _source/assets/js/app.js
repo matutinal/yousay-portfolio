6a67ab988e07a7df3599d36948764a70f09790b6
martin-laptop <martin@matutinal.fr>
2026-08-03 19:37:45 +0200

    footer

--- _source/assets/js/app.js
+++ _source/assets/js/app.js
@@ -6,3 +6,8 @@ import PopoverMenu from './_elements/popover-menu.js';
 import RandomShapes from './_elements/random-shapes.js';
 import SmallDetails from './_elements/small-details.js';
 import ThemePicker from './_elements/theme-picker.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const el = document.getElementById('current-year');
+  if (el) el.textContent = new Date().getFullYear();
+});
